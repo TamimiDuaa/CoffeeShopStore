65622bca84179ae102d31d2bf69037010c8d5a49
TamimiDuaa <tamimiduaa18@gmail.com>
2024-10-13 13:01:03 -0400

    deploy the app

--- src/context/CoffeeContext.jsx
+++ src/context/CoffeeContext.jsx
@@ -7,7 +7,7 @@ export const CoffeeContext = createContext();
 // Create the provider
 export const CoffeeProvider = ({ children }) => {
     const [state, dispatch] = useReducer(orderReducer, { orders: [] });
-
+    
     return (
         <CoffeeContext.Provider value={{ state, dispatch }}>
             {children}
