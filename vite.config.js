21addc0a24c8f24d6992c35df459067c69737fc7
TamimiDuaa <tamimiduaa18@gmail.com>
2024-10-13 13:28:58 -0400

    test deploying

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/CoffeeShopStore/',  // Add your repository name here
   plugins: [react()],
 })
